ecdfa8de850aa769474719c06b7106319d1f1f0f
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-19 17:54:02 -0300

    Update App.jsx

--- team-member-alocation/src/App.jsx
+++ team-member-alocation/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css';
 import Content from './Content';
+import Employeed from './Employees';
 import Footer from './Footer';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Employeed></Employeed>
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
